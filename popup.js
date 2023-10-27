e84217e0cb9096f94b8c108b940c6c58dc4f7dbe
olespersonal <olespersonal12@gmail.com>
2023-10-27 21:02:18 +0300

    feat: custom messages list

--- popup.js
+++ popup.js
@@ -1,8 +1,15 @@
 const websiteList = document.getElementById("website-list");
 const websiteInput = document.getElementById("website-input");
+const messageInput = document.getElementById("message-input");
+const messageList = document.getElementById("message-list");
 const addBtn = document.getElementById("add-btn");
+const addMessageBtn = document.getElementById("add-message-btn");
 const themeBtn = document.getElementById("theme-btn");
 const themeStylesheet = document.getElementById("theme-stylesheet");
+
+const deleteBtn = `<div class="delete-btn">remove</div>`;
+
+// Theme code
 
 themeBtn.addEventListener("click", () => {
   chrome.storage.local.get(["isDarkTheme"], (result) => {
@@ -28,7 +35,7 @@ chrome.storage.local.get(["isDarkTheme"], (result) => {
   }
 });
 
-const deleteBtn = `<div class="delete-btn">remove</div>`;
+// Get a list
 
 const getWebsiteList = async () => {
   return new Promise((resolve) => {
@@ -38,31 +45,15 @@ const getWebsiteList = async () => {
   });
 };
 
-function handleKeyPress(event) {
-  if (event.key === "Enter") {
-    addWebsiteUrl();
-  }
-}
-
-websiteInput.addEventListener("keydown", handleKeyPress);
-
-const deleteWebsiteListItem = async (event) => {
-  if (event.target.classList.contains("delete-btn")) {
-    // Find the sibling <p> element of the clicked "delete-cion" div
-    const siblingP = event.target.previousElementSibling;
-
-    if (siblingP && siblingP.tagName === "SPAN") {
-      const paragraphText = siblingP.textContent;
-      const list = await getWebsiteList();
-      await chrome.storage.local.set({
-        websiteList: list.filter((item) => item !== paragraphText),
-      });
-      displayWebsiteList();
-    }
-  }
+const getMessageList = async () => {
+  return new Promise((resolve) => {
+    chrome.storage.local.get(["messageList"], (result) => {
+      resolve(result.messageList ?? []);
+    });
+  });
 };
 
-websiteList.addEventListener("click", deleteWebsiteListItem);
+// Display content
 
 const displayWebsiteList = async () => {
   const list = await getWebsiteList();
@@ -74,12 +65,79 @@ const displayWebsiteList = async () => {
 
 displayWebsiteList();
 
+const displayMessageList = async () => {
+  const list = await getMessageList();
+  const content = list.map(
+    (item) => `<div class="list-item"><span>${item}</span>${deleteBtn}</div>`
+  );
+  messageList.innerHTML = content.join("");
+};
+
+displayMessageList();
+
+// List item manipulations
+
 const addWebsiteUrl = async () => {
   const inputText = document.getElementById("website-input").value;
-  const list = await getWebsiteList();
-  await chrome.storage.local.set({ websiteList: [...list, inputText] });
   websiteInput.value = "";
+
+  const list = await getWebsiteList();
+
+  if (list.includes(inputText)) return;
+
+  await chrome.storage.local.set({ websiteList: [...list, inputText] });
   displayWebsiteList();
 };
 
+const addMessage = async () => {
+  const inputText = document.getElementById("message-input").value;
+  messageInput.value = "";
+
+  const list = await getMessageList();
+
+  if (list.includes(inputText)) return;
+
+  await chrome.storage.local.set({ messageList: [...list, inputText] });
+  displayMessageList();
+};
+
+const deleteListItem = async (event) => {
+  if (event.target.classList.contains("delete-btn")) {
+    // Find the sibling <p> element of the clicked "delete-cion" div
+    const siblingP = event.target.previousElementSibling;
+
+    if (siblingP && siblingP.tagName === "SPAN") {
+      const paragraphText = siblingP.textContent;
+      if (event.currentTarget.id === "website-list") {
+        const list = await getWebsiteList();
+        await chrome.storage.local.set({
+          websiteList: list.filter((item) => item !== paragraphText),
+        });
+        displayWebsiteList();
+      } else {
+        const list = await getMessageList();
+        await chrome.storage.local.set({
+          messageList: list.filter((item) => item !== paragraphText),
+        });
+        displayMessageList();
+      }
+    }
+  }
+};
+
+// Event listeners
+
+const handleKeyPress = (event) => {
+  if (event.key === "Enter") {
+    console.log("event.target", event.target.id);
+    if (event.target.id === "website-input") addWebsiteUrl();
+    else addMessage();
+  }
+};
+
+websiteInput.addEventListener("keydown", handleKeyPress);
+messageInput.addEventListener("keydown", handleKeyPress);
+websiteList.addEventListener("click", deleteListItem);
+messageList.addEventListener("click", deleteListItem);
 addBtn.addEventListener("click", addWebsiteUrl);
+addMessageBtn.addEventListener("click", addMessage);
--- redirect-page.js
+++ redirect-page.js
@@ -1,19 +1,16 @@
-const messages = [
-  "This website is blocked for a reason",
-  "Nope",
-  "Remember why you blocked this website?",
-  "It's ruining your life",
-  "Don't be an idiot",
-  "You don't approve this website, don't remember?",
-];
-
 document.addEventListener("DOMContentLoaded", function () {
-  chrome.storage.local.get(["isDarkTheme"], (result) => {
+  chrome.storage.local.get(["isDarkTheme", "messageList"], (result) => {
     document.getElementById("body").style.backgroundColor = result.isDarkTheme
       ? "rgb(38, 39, 38)"
       : "background-color: rgb(91, 204, 91);";
+
+    const text = document.getElementById("text");
+
+    text.innerHTML =
+      Array.isArray(result.messageList) && result.messageList.length
+        ? result.messageList[
+            Math.floor(Math.random() * result.messageList.length)
+          ]
+        : "You blocked this website";
   });
 });
-
-const text = document.getElementById("text");
-text.innerHTML = messages[Math.floor(Math.random() * messages.length)];
